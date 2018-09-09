f3f57e5ec39c7fd5b8dd0306e72ff992b7b65a70
OyewoleOni <onioyewole1910@gmail.com>
2018-09-09 23:46:24 +0100

    Second

--- testVersion/src/app/app.module.ts
+++ testVersion/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AppRoutingModule, routingComponent } from './app-routing.Module';
 import { PostService } from './post.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -6,15 +7,23 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
 import {HttpClientModule} from '@angular/common/http';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+// import { DepartmentListComponent } from './department-list/department-list.component';
+// import { EmployeeListComponent } from './employee-list/employee-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent
+    PostComponent,
+    // DepartmentListComponent,
+    // EmployeeListComponent,
+    routingComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [PostService],
   bootstrap: [AppComponent]
